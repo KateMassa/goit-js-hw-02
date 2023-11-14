0c4bdde0e806dd9452ce92e289c4b7d658a8a7b7
KateMassa <catherineorlova27@gmail.com>
2023-11-14 22:12:35 +0100

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,13 @@
-function checkForSpam(message) {}
+function checkForSpam(message) {
+  const spamText = message.toLowerCase().includes('spam');
+  const saleText = message.toLowerCase().includes('sale');
+  return spamText || saleText;
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
